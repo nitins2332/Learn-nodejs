2643ea1c1b3704d58a18485214e0cb88ba8d6754
Nitins <nitins2332@gmail.com>
2022-06-19 18:02:02 +0530

    learn node js

--- myfile.js
+++ myfile.js
@@ -49,3 +49,92 @@
 // });
 // });
 //   .listen(8080);
+
+// const name = "thapa";
+// console.log(name);
+
+// const fs = require("fs");
+
+// fs.writeFileSync("read.txt", "welcome to my channel");
+// fs.writeFileSync("read.txt", "welcome to my channel, welcome again");
+
+// fs.appendFileSync("read.txt", "hello this is nitins");
+
+// fs.readFileSync("read.txt");
+// const buf_data = fs.readFileSync("read.txt");
+// console.log(buf_data);
+
+// org_data = buf_data.toString();
+
+// console.log(org_data);
+
+// fs.renameSync("read.txt", "readwrite.txt");
+
+// const fs = require("fs");
+
+// const readFileData = fs.readFile("readwrite.txt", "content-type"= "txt/html")
+
+// console.log(readFileData);
+// fs.readFile("readwrite.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// fs.rm("readwrite.txt");
+// fs.writeFileSync("readwrite.txt", "hello welcome again in this file");
+
+// fs.readFile("readwrite.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// const fs = require("fs");
+
+// fs.writeFile("read.txt", "today is my day :", (err) => {
+//   console.log("file is created");
+// });
+
+// fs.appendFile("read.txt", "this is write by nitins", (err) => {
+//   console.log("taks is complete");
+// });
+
+// fs.readFile("read.txt", "utf8", (err, data) => {
+//   console.log(data);
+// });
+
+// const fs = require("fs");
+// const data = fs.readFileSync("read.txt", "utf8");
+// console.log(data);
+// fs.readFile("read.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+// console.log("after the data");
+
+// fs.mkdir("thapa", (err) => {
+//   console.log("folder Created!");
+// });
+// fs.writeFile("thapa/read.txt", "this file is in the folder thapa", (err) => {
+//   console.log("file is created!");
+// });
+
+// fs.appendFile("./thapa/read.txt", " this is second line", (err) => {
+//   console.log("file is updated!");
+// });
+
+// fs.readFile("./thapa/read.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// fs.rename("./thapa/read.txt", "./thapa/mybio.txt", (err) => {
+//   console.log("Rename the file");
+// });
+
+// fs.unlink("./thapa/mybio.txt", (err) => {
+//   console.log("file is deleted");
+// });
+
+// fs.rmdir("./thapa", (err) => {
+//   console.log("folder is deleted");
+// });
