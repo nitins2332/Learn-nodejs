7cf2e047f4f5e85970540f1dc4495e5eab1c2dc9
Nitins <nitins2332@gmail.com>
2022-06-20 18:19:17 +0530

    Routing , Handled HTTP requests

--- myfile.js
+++ myfile.js
@@ -204,9 +204,20 @@
 // console.log(name);
 
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the other sides");
+  //   console.log(req.url);
+  if (req.url == "/") {
+    res.end("Hello from the home side");
+  } else if (req.url == "/about") {
+    res.end("Hello from the about side");
+  } else if (req.url == "/contact") {
+    res.end("Hello from the contact side");
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end("<h1>page not found</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
