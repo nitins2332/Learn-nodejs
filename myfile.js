91d551301b44cabde329cf7735ffb7297ea1ebf6
Nitins <nitins2332@gmail.com>
2022-06-21 23:47:02 +0530

    Project : dynamic quotes website

--- myfile.js
+++ myfile.js
@@ -325,30 +325,28 @@
 
 // myName(200, ok)
 
-const fs = require("fs");
-const http = require("http");
+// const fs = require("fs");
+// const http = require("http");
 
-const server = http.createServer();
+// const server = http.createServer();
 
-server.on("request", (req, res) => {
-  // var fs = require("fs");
-  // fs.readFile("read.txt", (err, data) => {
-  //   if (err) return console.log(err);
-  //   res.end(data.toString());
-  // });
+// server.on("request", (req, res) => {
+// var fs = require("fs");
+// fs.readFile("read.txt", (err, data) => {
+//   if (err) return console.log(err);
+//   res.end(data.toString());
+// });
+// 2nd way
+// const rstream = fs.createReadStream("read.txt");
+// rstream.on("data", (chunkdata) => {
+//   res.write(chunkdata);
+// });
+// rstream.on("end", () => {
+//   res.end();
+// });
+// 3rd way
+// const rstream = fs.createReadStream("read.txt");
+// rstream.pipe(res);
+// });
 
-  // 2nd way
-  // const rstream = fs.createReadStream("read.txt");
-  // rstream.on("data", (chunkdata) => {
-  //   res.write(chunkdata);
-  // });
-  // rstream.on("end", () => {
-  //   res.end();
-  // });
-
-  // 3rd way
-  const rstream = fs.createReadStream("read.txt");
-  rstream.pipe(res);
-});
-
-server.listen(8000, "127.0.0.1");
+// server.listen(8000, "127.0.0.1");
